c1184c1262770b63394fcc106a13cdd41fe59b1e
wanjohi lewis <wanjohilewi7@gmail.com>
2024-10-11 01:11:51 +0300

    added the functional response class

--- CryptoVase/src/main/java/DevLewi/CryptoVase/service/ChatbotServiceImpl.java
+++ CryptoVase/src/main/java/DevLewi/CryptoVase/service/ChatbotServiceImpl.java
@@ -108,19 +108,36 @@ public class ChatbotServiceImpl implements ChatbotService{
                                                         .put("properties", new JSONObject()
                                                                 .put("currencyName", new JSONObject()
                                                                         .put("type", "STRING")
-                                                                        .put("description", "The currency name, id, symbol.")
+                                                                        .put(
+                                                                                "description",
+                                                                                "The currency name, " +
+                                                                                        "id, symbol.")
                                                                 )
                                                                 .put("currencyDate", new JSONObject()
                                                                         .put("type", "STRING")
                                                                         .put("description",
-                                                                                "Currency Data id, symbol, name, image, current_price, " +
-                                                                                        "market_cap, market_cap_rank, fully_diluted_valuation, " +
-                                                                                        "total_volume, high_24h, low_24h, price_change_24h, " +
-                                                                                        "price_change_percentage_24h, market_cap_change_24h, " +
-                                                                                        "market_cap_change_percentage_24h, circulating_supply, " +
-                                                                                        "total_supply, max_supply, ath, ath_change_percentage, " +
-                                                                                        "ath_date, atl, atl_change_percentage, atl_date, last_updated."
-                                                                        )
+                                                                                "Currency Data id, " +
+                                                                                        "symbol, " +
+                                                                                        "name, " +
+                                                                                        "image, " +
+                                                                                        "current_price, " +
+                                                                                        "market_cap, " +
+                                                                                        "market_cap_rank, " +
+                                                                                        "fully_diluted_valuation, " +
+                                                                                        "total_volume, high_24h, " +
+                                                                                        "low_24h, price_change_24h, " +
+                                                                                        "price_change_percentage_24h, " +
+                                                                                        "market_cap_change_24h, " +
+                                                                                        "market_cap_change_percentage_24h, " +
+                                                                                        "circulating_supply, " +
+                                                                                        "total_supply, " +
+                                                                                        "max_supply, " +
+                                                                                        "ath, " +
+                                                                                        "ath_change_percentage, " +
+                                                                                        "ath_date, " +
+                                                                                        "atl, " +
+                                                                                        "atl_change_percentage, " +
+                                                                                        "atl_date, last_updated.")
                                                                 )
                                                         )
                                                         .put("required", new JSONArray()
